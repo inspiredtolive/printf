63cc9ecae7907b7790d105cbb7f60d003e4a3447
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-07-28 05:43:47 +0000

    Added print number functionality

---
+++ _print_number.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _print_number - a function that prints an integer.
+ * @n: input integer
+ */
+
+int _print_number(int n)
+{
+	unsigned int m;
+	int count = 0;
+
+	if (n >= 0)
+	{
+		if (n >= 10)
+			_print_number(n / 10);
+		_putchar (n % 10 + '0');
+		count++;
+	}
+	else
+	{
+		_putchar('-');
+		m = -n;
+		if (m >= 10)
+			_print_number(m / 10);
+		_putchar(m % 10 + '0');
+		count++;
+	}
+	return (count);
+}
--- _printf.c
+++ _printf.c
@@ -13,6 +13,8 @@ int _printf(char *format, ...)
 	print_fn getfn[] = {
 		{"%c", print_char},
 		{"%s", print_string},
+		{"%d", print_num},
+		{"%i", print_num},
 		{NULL, NULL}
 	};
 
