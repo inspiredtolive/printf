359451d81bf6eac850f9997bf197e413a9256c6d
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-07-29 23:37:50 +0000

    Changed allocation of hex using pointer rather than array

--- _print_hexlower.c
+++ _print_hexlower.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 
 /**
- *  * _print_hex - a function that prints a hexadecimal number
+ *  * _print_hexlower - a function that prints a hexadecimal number
  *   * @number: input number
  *    * Return: length of hexadecimal number
  *     */
 
-int _print_hex(unsigned int number)
+int _print_hexlower(unsigned int number)
 {
 	int i, j, remainder, count = 0;
 	unsigned int num_copy = number;
-	
+	char *hexadecimal;
+
 	if (number == 0)
 		return (_putchar('0'));
 
@@ -19,23 +20,24 @@ int _print_hex(unsigned int number)
 		num_copy /= 16;
 		count++;
 	}
-	char hexArray[count];
+	hexadecimal = malloc(count);
+
 	for (i = 0; number != 0; i++)
 	{
 		remainder = 0;
 		remainder = number % 16;
 		if (remainder < 10)
 		{
-			hexArray[i] = remainder + '0';
+			hexadecimal[i] = remainder + '0';
 		}
 		else
 		{
-			hexArray[i] = remainder + 'W';
+			hexadecimal[i] = remainder + 'W';
 		}
 		number /= 16;
 	}
 	for (j = i - 1; j >= 0; j--)
-		_putchar(hexArray[j]);
+		_putchar(hexadecimal[j]);
 
 	return (count);
 }
--- print_hexlower.c
+++ print_hexlower.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 
 /**
- *  * print_hex - prints a hexadecimal
- *   * @format: format to print hexadecimal
- *    * @args: va_list that contains the hexadecimal to print
- *     * Return: number of digits printed
- *      */
-int print_hex(char *format, va_list args)
+ * print_hexlower - prints a hexadecimal
+ * @format: format to print hexadecimal
+ * @args: va_list that contains the hexadecimal to print
+ * Return: number of digits printed
+ */
+int print_hexlower(char *format, va_list args)
 {
 	unsigned int number = va_arg(args, unsigned int);
 	int count;
 	(void)format;
 
-	count = _print_hex(number);
+	count = _print_hexlower(number);
 	return (count);
 }
