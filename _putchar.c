62219924fb08c374e2bf3646e064d5b541792987
Marco Chan <inspiredtolive0@gmail.com>
2019-07-30 12:05:43 -0700

    Loads characters into buffer before calling write

--- _putchar.c
+++ _putchar.c
@@ -7,5 +7,25 @@
  */
 int _putchar(char c)
 {
+	static int bufferCount;
+	static char buffer[1024];
+
+	if (c == -1)
+	{
+		bufferCount = 0;
+		return (0);
+	}
+	if (c == -2 || bufferCount == 1024)
+	{
+		write(1, buffer, bufferCount);
+		bufferCount = 0;
+		return (0);
+	}
+	if (c != -1 && c != -2)
+	{
+		buffer[bufferCount] = c;
+		bufferCount++;
+		return(1);
+	}
 	return (write(1, &c, 1));
 }
