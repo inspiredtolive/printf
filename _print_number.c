344ed913ba4e4b57548f252bc65b25678f39c7ca
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-07-30 10:00:39 +0000

    Improved _print_number code

--- _print_number.c
+++ _print_number.c
@@ -13,19 +13,17 @@ int _print_number(int n)
 
 	count = _count_digits(n);
 
-	if (n >= 0)
-	{
-		if (n >= 10)
-			_print_number(n / 10);
-		_putchar (n % 10 + '0');
-	}
-	else
+	if (n < 0)
 	{
 		_putchar('-');
 		m = -n;
-		if (m >= 10)
-			_print_number(m / 10);
-		_putchar(m % 10 + '0');
 	}
+	else
+		m = n;
+
+	if (m >= 10)
+		_print_number(m / 10);
+	_putchar(m % 10 + '0');
+
 	return (count);
 }
